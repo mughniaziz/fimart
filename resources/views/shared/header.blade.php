a9055167d89e07483ab9ae184e8daa7d9cf10339
Mughni Aziz <mughniaziz04@gmail.com>
2019-05-25 07:07:38 +0700

    Tambah menu Admin

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -8,11 +8,20 @@
             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
         </li>
         @else
+        @if (Auth::user()->hasRole('admin'))
+            <li class="nav-item">
+                <a href="#" class="nav-link">Tambah Kategori</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Tambah Barang</a>
+            </li>
+        @endif
         <li class="nav-item dropdown">
             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                 {{ Auth::user()->name }} <span class="caret"></span>
             </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+            <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
